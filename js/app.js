a8e90382c09823836d6b1f6091f43afeba4ac7aa
Ali <aisaev@codal.com>
2021-07-03 16:08:37 +0300

    test

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log('js file')
+const skills = [
+  {name:'HTML', level: '100%'},
+  {name:'CSS', level: '100%'},
+  {name:'Sass', level: '100%'},
+  {name:'JavaScript', level: '90%'},
+  {name:'React JS', level: '90%'},
+  {name:'Next JS', level: '90%'},
+  {name:'GraphQL', level: '60%'},
+  {name:'Node JS', level: '30%'},
+  {name:'Express', level: '30%'},
+  {name:'Shopify', level: '90%'},
+  {name:'Liquid', level: '70%'},
+  {name:'Builder.io', level: '100%'},
+  {name:'GitHub', level: '100%'},
+  {name:'JIRA', level: '100%'},
+  {name:'Asana', level: '100%'},
+]
+
+const skillsContainer = document.querySelector('.skills')
+
+skills.map((el, i)=> {
+  const div = document.createElement('div')
+  div.setAttribute('class', 'skills__item')
+  div.innerHTML = `
+    <span class='skills__item_name'>${el.name}</span>
+    <span class='skills__item_level'>
+      <div class='skills__item_level--value' style='width:${el.level};'></div>
+    </span>
+    `
+
+  skillsContainer.appendChild(div)
+})
